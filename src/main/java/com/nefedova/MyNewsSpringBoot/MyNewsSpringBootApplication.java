1218d422b31419cac28a825dc7bfb0e8cb7a7e7a
Ekaterina Nefedova <ekaterina.nefedova@orioninc.com>
2022-03-22 22:24:06 +0300

    Initialize project

---
+++ src/main/java/com/nefedova/MyNewsSpringBoot/MyNewsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.nefedova.MyNewsSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNewsSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyNewsSpringBootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/nefedova/MyNewsSpringBoot/MyNewsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nefedova.MyNewsSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNewsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
